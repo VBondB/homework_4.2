c2f942a85a3a99b654b341488b9d252f686230fa
VBondB <Bvv051270@gmail.com>
2025-01-30 22:58:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введіть перше число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введіть друге число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введіть дію (+, -, *, /): ");
+char operation = Console.ReadLine() [0];
+
+int result = 0; 
+
+    switch (operation)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
+
+Console.WriteLine("Результат: " + result);
